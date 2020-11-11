c1e4a74e12513c01324b04f39a8bb3ea5854ffa2
Manideep Lanka <manideep.l@zen3.com>
2020-11-12 01:39:32 +0530

    feat: auth

--- src/Application.kt
+++ src/Application.kt
@@ -1,26 +1,49 @@
 package manlan
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.application.*
-import io.ktor.features.ContentNegotiation
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
+import io.ktor.features.*
 import io.ktor.jackson.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import org.slf4j.LoggerFactory
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     install(ContentNegotiation) {
-        jackson {  }
+        jackson { }
     }
 
+    install(Authentication) {
+        jwt {
+            realm = "jwt.realm"
+            verifier(makeJWTVerifier("jwt.domain", "jwt.audience"))
+            validate { jwtCredential ->
+                if (jwtCredential.payload.audience.contains("jwt.audience")) JWTPrincipal(jwtCredential.payload) else null
+            }
+        }
+    }
 
     routing {
+        authenticate {
             get("/books") {
                 val input: InputForBooks = call.receive()
                 call.respond(DbRepo.getReaders(input.radius, input.geoLocation))
             }
+        }
+
+        post("/login") {
+            
+        }
     }
+}
+
+fun makeJWTVerifier(issuer: String, audience: String): JWTVerifier {
+    return JWT.require(Algorithm.HMAC256("secret")).withAudience(audience).withIssuer(issuer).build()
 }
 
